87a87ddc2fefaaaae3cbaa0605883e7706a6f335
James Pansarasa <james.pansarasa@gmail.com>
2019-06-16 12:42:47 -0400

    Added PrintStatistics method

--- Producer/Producer.cs
+++ Producer/Producer.cs
@@ -7,22 +7,25 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Client.Transport.Mqtt;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace IotEdge
 {
     class Producer
     {
+        private static int messageCount = 0;
+        private static int methodCount = 0;
         public static async Task Main(string[] args)
         {
 #if DEBUG
-            int count = 0;
-            while (!System.Diagnostics.Debugger.IsAttached && (count++ < 60))
+            int count = 60;
+            do
             {
-                Thread.Sleep(1000);
-                Console.WriteLine("Waiting for debugger");
+                if (0 == count % 10) Console.WriteLine($"Waiting for debugger. {count} seconds left before continuing");
+                await Task.Delay(1000);
             }
+            while (!System.Diagnostics.Debugger.IsAttached && (count-- > 0));
 #endif
+
             Console.WriteLine("{0:O} - Starting", DateTime.Now);
             ITransportSettings[] settings = { new MqttTransportSettings(TransportType.Mqtt_Tcp_Only) };
             using (var moduleClient = await ModuleClient.CreateFromEnvironmentAsync(settings))
@@ -35,7 +38,9 @@ namespace IotEdge
                 // Wait until the app unloads or is cancelled
                 AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
                 Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
-                await WhenCancelled(cts.Token);
+                await Task.WhenAll(
+                    WhenCancelled(cts.Token),
+                    Task.Run(async () => await PrintStatistics(cts.Token)));
             }
             Console.WriteLine("{0:O} - Exiting", DateTime.Now);
         }
@@ -46,13 +51,14 @@ namespace IotEdge
             cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
             return tcs.Task;
         }
+
         private static byte[] GetPayload() => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { UtcTime = DateTime.UtcNow }));
 
         private static Task<MethodResponse> GetTimeMethod(MethodRequest methodRequest, object userContext)
         {
-            Console.WriteLine("{0:O} - GetTimeMethod", DateTime.Now);
             try
             {
+                Interlocked.Increment(ref methodCount);
                 var response = new MethodResponse(GetPayload(), 200);
                 return Task.FromResult(response);
             }
@@ -65,9 +71,9 @@ namespace IotEdge
 
         private static async Task<MessageResponse> GetTimeMessage(Message message, object userContext)
         {
-            Console.WriteLine("{0:O} - GetTimeMessage", DateTime.Now);
             try
             {
+                Interlocked.Increment(ref messageCount);
                 var moduleClient = (ModuleClient)userContext;
                 var response = new Message(GetPayload()) { CorrelationId = message.CorrelationId };
                 await moduleClient.SendEventAsync(nameof(GetTimeMessage), response);
@@ -79,16 +85,28 @@ namespace IotEdge
                 return MessageResponse.Abandoned;
             }
         }
-        
-        private static void LogException(Exception ex, [CallerMemberName] string memberName = "") {
-                Console.WriteLine($"{DateTime.Now} - {memberName}: Caught exception {ex.GetType().Name} - {ex.Message}");
-                var inner = ex.InnerException;
-                while (inner != null) {
-                    Console.WriteLine($"Inner exception {ex.InnerException.GetType().Name} - {ex.InnerException.Message}");
-                    inner = inner.InnerException;
-                }
-                Console.WriteLine(ex.StackTrace);
+
+        private static void LogException(Exception ex, [CallerMemberName] string memberName = "")
+        {
+            Console.WriteLine($"{DateTime.Now} - {memberName}: Caught exception {ex.GetType().Name} - {ex.Message}");
+            var inner = ex.InnerException;
+            while (inner != null)
+            {
+                Console.WriteLine($"Inner exception {ex.InnerException.GetType().Name} - {ex.InnerException.Message}");
+                inner = inner.InnerException;
+            }
+            Console.WriteLine(ex.StackTrace);
         }
 
+        private static async Task PrintStatistics(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay((int)TimeSpan.FromMinutes(1).TotalMilliseconds, cancellationToken);
+                var mths = Interlocked.Exchange(ref methodCount, 0);
+                var msgs = Interlocked.Exchange(ref messageCount, 0);
+                Console.WriteLine("{0:O} - {1} Method calls, {2} Message calls ", DateTime.Now, mths, msgs);
+            }
+        }
     }
 }
