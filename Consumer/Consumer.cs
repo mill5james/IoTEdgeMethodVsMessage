3c7b3ea14608c50c40612f43d5cf34885f8aa42e
James Pansarasa <james.pansarasa@gmail.com>
2019-06-16 10:47:14 -0400

    Added ability to disable messages or method calls

--- Consumer/Consumer.cs
+++ Consumer/Consumer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
 using System.Text;
@@ -7,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Client.Transport.Mqtt;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace IoTEdge
@@ -15,6 +15,10 @@ namespace IoTEdge
     public class Consumer
     {
         private static string DeviceId { get; } = Environment.GetEnvironmentVariable("IOTEDGE_DEVICEID");
+        private static bool EnableMethod = true;
+        private static bool EnableMessage = true;
+        private static readonly string[] TrueStrings = { "y", "yes", "true", "on", "1" };
+
         public static async Task Main(string[] args)
         {
 #if DEBUG
@@ -25,7 +29,13 @@ namespace IoTEdge
                 await Task.Delay(1000);
             }
 #endif
+
             Console.WriteLine("{0:O} - Starting", DateTime.Now);
+
+            EnableMessage = TrueStrings.Contains(Environment.GetEnvironmentVariable("EnableMessage") ?? bool.TrueString, StringComparer.OrdinalIgnoreCase);
+            EnableMethod = TrueStrings.Contains(Environment.GetEnvironmentVariable("EnableMethod") ?? bool.TrueString, StringComparer.OrdinalIgnoreCase);
+
+            Console.WriteLine("{0:O} - EnableMessage:{1} EnableMethod:{2}", DateTime.Now, EnableMessage, EnableMethod);
             ITransportSettings[] settings = { new MqttTransportSettings(TransportType.Mqtt_Tcp_Only) };
             using (var moduleClient = await ModuleClient.CreateFromEnvironmentAsync(settings))
             using (var resetEvent = new ManualResetEventSlim(false))
@@ -39,8 +49,8 @@ namespace IoTEdge
                 Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
 
                 await Task.WhenAll(
-                    Task.Run(async () => await GetTimeMethod(moduleClient, cts.Token)),
-                    Task.Run(async () => await RequestTimeMessage(moduleClient, resetEvent, cts.Token)),
+                    EnableMethod ? Task.Run(async () => await GetTimeMethod(moduleClient, cts.Token)) : Task.CompletedTask,
+                    EnableMessage ? Task.Run(async () => await RequestTimeMessage(moduleClient, resetEvent, cts.Token)) : Task.CompletedTask,
                     Task.Run(() => PrintStatistics(cts.Token)),
                     WhenCancelled(cts.Token));
             }
@@ -112,14 +122,16 @@ namespace IoTEdge
             }
         }
 
-        private static void LogException(Exception ex, [CallerMemberName] string memberName = "") {
-                Console.WriteLine($"{DateTime.Now} - {memberName}: Caught exception {ex.GetType().Name} - {ex.Message}");
-                var inner = ex.InnerException;
-                while (inner != null) {
-                    Console.WriteLine($"Inner exception {ex.InnerException.GetType().Name} - {ex.InnerException.Message}");
-                    inner = inner.InnerException;
-                }
-                Console.WriteLine(ex.StackTrace);
+        private static void LogException(Exception ex, [CallerMemberName] string memberName = "")
+        {
+            Console.WriteLine($"{DateTime.Now} - {memberName}: Caught exception {ex.GetType().Name} - {ex.Message}");
+            var inner = ex.InnerException;
+            while (inner != null)
+            {
+                Console.WriteLine($"Inner exception {ex.InnerException.GetType().Name} - {ex.InnerException.Message}");
+                inner = inner.InnerException;
+            }
+            Console.WriteLine(ex.StackTrace);
         }
 
         private static ConcurrentQueue<(string Method, DateTime Begin, DateTime Produced, DateTime End)> measurements = new ConcurrentQueue<(string Method, DateTime Begin, DateTime Produced, DateTime End)>();
@@ -128,7 +140,8 @@ namespace IoTEdge
 
         private static void PrintStatistics(CancellationToken cancellationToken)
         {
-            Func<(long min, long max, long avg), long, (long,long,long)> computeStats = (tuple, val) => { 
+            Func<(long min, long max, long avg), long, (long, long, long)> computeStats = (tuple, val) =>
+            {
                 tuple.min = Math.Min(tuple.min, val);
                 tuple.max = Math.Max(tuple.max, val);
                 tuple.avg += val;
@@ -138,12 +151,12 @@ namespace IoTEdge
             while (!cancellationToken.IsCancellationRequested)
             {
                 var stats = (
-                    Method: (Request:   (min: long.MaxValue, max: long.MinValue, avg: 0L),
-                            Response:   (min: long.MaxValue, max: long.MinValue, avg: 0L),
-                            Total:      (min: long.MaxValue, max: long.MinValue, avg: 0L)),
-                    Message:(Request:   (min: long.MaxValue, max: long.MinValue, avg: 0L),
-                            Response:   (min: long.MaxValue, max: long.MinValue, avg: 0L),
-                            Total:      (min: long.MaxValue, max: long.MinValue, avg: 0L)));
+                    Method: (Request: (min: long.MaxValue, max: long.MinValue, avg: 0L),
+                            Response: (min: long.MaxValue, max: long.MinValue, avg: 0L),
+                            Total: (min: long.MaxValue, max: long.MinValue, avg: 0L)),
+                    Message: (Request: (min: long.MaxValue, max: long.MinValue, avg: 0L),
+                            Response: (min: long.MaxValue, max: long.MinValue, avg: 0L),
+                            Total: (min: long.MaxValue, max: long.MinValue, avg: 0L)));
 
                 Thread.Sleep((int)TimeSpan.FromMinutes(1).TotalMilliseconds);
                 var count = measurements.Count;
@@ -151,13 +164,15 @@ namespace IoTEdge
                 int i = 0, messageCount = 0, methodCount = 0;
                 while ((i++ < count) && measurements.TryDequeue(out var measure))
                 {
-                    if (measure.Method == nameof(GetTimeMessage)) {
+                    if (measure.Method == nameof(GetTimeMessage))
+                    {
                         messageCount++;
                         stats.Message.Request = computeStats(stats.Message.Request, (measure.Produced - measure.Begin).Ticks);
                         stats.Message.Response = computeStats(stats.Message.Response, (measure.End - measure.Produced).Ticks);
                         stats.Message.Total = computeStats(stats.Message.Total, (measure.End - measure.Begin).Ticks);
                     }
-                    else {
+                    else
+                    {
                         methodCount++;
                         stats.Method.Request = computeStats(stats.Method.Request, (measure.Produced - measure.Begin).Ticks);
                         stats.Method.Response = computeStats(stats.Method.Response, (measure.End - measure.Produced).Ticks);
@@ -167,17 +182,22 @@ namespace IoTEdge
                 Console.WriteLine($"{now:O} - {count} items in 1 minute");
                 Console.WriteLine("                | Min           | Max           | Avg           |");
                 Console.WriteLine("--------+-------+---------------+---------------+---------------|");
-                Console.WriteLine("        | C->P  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Request.min), new TimeSpan(stats.Message.Request.max), new TimeSpan(stats.Message.Request.avg / count));
-                Console.WriteLine("Message | P->C  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Response.min), new TimeSpan(stats.Message.Response.max), new TimeSpan(stats.Message.Response.avg / count));
-                Console.WriteLine("{3,7:G} | Total | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Total.min), new TimeSpan(stats.Message.Total.max), new TimeSpan(stats.Message.Total.avg / count), messageCount);
-                Console.WriteLine("--------+-------+---------------+---------------+---------------|");
-                Console.WriteLine("        | C->P  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Request.min), new TimeSpan(stats.Method.Request.max), new TimeSpan(stats.Method.Request.avg / count));
-                Console.WriteLine("Method  | P->C  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Response.min), new TimeSpan(stats.Method.Response.max), new TimeSpan(stats.Method.Response.avg / count));
-                Console.WriteLine("{3,7:G} | Total | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Total.min), new TimeSpan(stats.Method.Total.max), new TimeSpan(stats.Method.Total.avg / count), methodCount);
-                Console.WriteLine("--------+-------+---------------+---------------+---------------|");
-                
-            }
+                if (EnableMessage)
+                {
+                    Console.WriteLine("        | C->P  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Request.min), new TimeSpan(stats.Message.Request.max), new TimeSpan(stats.Message.Request.avg / count));
+                    Console.WriteLine("Message | P->C  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Response.min), new TimeSpan(stats.Message.Response.max), new TimeSpan(stats.Message.Response.avg / count));
+                    Console.WriteLine("{3,7:G} | Total | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Message.Total.min), new TimeSpan(stats.Message.Total.max), new TimeSpan(stats.Message.Total.avg / count), messageCount);
+                    Console.WriteLine("--------+-------+---------------+---------------+---------------|");
+                }
+                if (EnableMethod)
+                {
+                    Console.WriteLine("        | C->P  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Request.min), new TimeSpan(stats.Method.Request.max), new TimeSpan(stats.Method.Request.avg / count));
+                    Console.WriteLine("Method  | P->C  | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Response.min), new TimeSpan(stats.Method.Response.max), new TimeSpan(stats.Method.Response.avg / count));
+                    Console.WriteLine("{3,7:G} | Total | {0:mm\\:ss\\.fffffff} | {1:mm\\:ss\\.fffffff} | {2:%mm\\:ss\\.fffffff} |", new TimeSpan(stats.Method.Total.min), new TimeSpan(stats.Method.Total.max), new TimeSpan(stats.Method.Total.avg / count), methodCount);
+                    Console.WriteLine("--------+-------+---------------+---------------+---------------|");
+                }
 
+            }
         }
     }
 }
